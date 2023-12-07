6ecefe3d88032429885e69e2facb2d2cd7f7c440
Yasm-ina <boumarafyasmina1991@gmail.com>
2023-12-07 12:29:36 +0100

    wip Datepicker- win :  have separation between 2 date selected in datePickers

--- imports/ui/components/DateFliter.vue
+++ imports/ui/components/DateFliter.vue
@@ -1,6 +1,7 @@
 <template>
-        <v-date-picker v-model="actualValueDate" @update:model-value="dateUpdate" :color="datePickerColor" 
-        :is-date-filter-close="isDateFilterClose" :date="date">
+        <v-date-picker
+        v-model="actualValueDate" :color="datePickerColor" :is-date-filter-close="isDateFilterClose" 
+        :date="date" @update:model-value="dateUpdate">
         </v-date-picker>
 </template>
 <script setup>
@@ -21,27 +22,32 @@ const props = defineProps ({
   },
   date : {
     type: String,
-    default: new Date("").getTime(),
+    default: new Date(),
     required: true, 
+  },
+  id: {
+    type: Number,
+    default: 0,
+    required: true
   },
 });
 
-
-//--------------------DATA-----------------------
+//-----------------------DATA---------------------------
 // access VUETIFY - date composable 
-const dateVuetify = useDate()
-console.log('dateNOTconverted', dateVuetify.date(new Date()))
-
-//V-MODEL - bidirectionnel
-// const datePicker = ref(new Date())
-// console.log('datePicker', datePicker.value)
+const accessDateVuetify = useDate()
 
 const actualValueDate = ref(new Date())
 console.log('datePicker', actualValueDate.value)
+// const dateFormatted = actualValueDate.value.format()
+// console.log('formated', dateFormatted)
+console.log('dateNOTconverted', accessDateVuetify.date(new Date()))
+
+
 const newValue = ref(dateUpdate)
 console.log('BRRR', newValue.ref)
+
 //V-MODEL - update
-function dateUpdate(event){
+function dateUpdate(update){
   actualValueDate.value
   console.log('aurevoir', actualValueDate)
   // console.log('SALUT', event.target.value)
@@ -50,13 +56,6 @@ function dateUpdate(event){
   // }
   // console.log('dateUpdate', typeof newValue)
 }
-
-
-// //V-MODEL - update
-// function dateUpdate(newValue){
-//   actualValueDate.value = newValue
-//   console.log('dateUpdate', typeof newValue)
-// }
 
 // const actualValueDate = computed({
 //   get(){
@@ -67,20 +66,10 @@ function dateUpdate(event){
 //   }
 // })
 
-function DATE (){
-//  tsMilliseconds = dayjsMixins.methods.unixSecondTs
-
-DateFinaleFormat = Number(datePicker.value = new Date())
-console.log('DAteFinalFormat', DateFinaleFormat)
-//  console.log('datePicker', datePicker.value)
-// console.log('TSUNIX', tsMilliseconds(dateUpdate()))
-  
-}
-
-//---------------------TS------------------------
+//------------------------TS--------------------------
 const tsMilliseconds = dayjsMixins.methods.unixSecondTs
 console.log('TSUNIX', tsMilliseconds(dateUpdate()))
-//-------------------METHODS----------------------
+//---------------------METHODS------------------------
 /**
  * @param {Object} datePicker
  * @function
@@ -92,8 +81,7 @@ if(datePicker.value != dateUpdate.newValue )
 newValue 
 }
 
-
-//-------------TODO-----------------------
+//---------------------TODO-------------------------
 //getDiff (date: unknown, comparing: unknown, unit: string) => number
 //Returns the difference between two dates in the specified unit.
 </script>
--- imports/ui/components/menuHeader.vue
+++ imports/ui/components/menuHeader.vue
@@ -1,28 +1,27 @@
 <template>
        <v-card class="mx-auto" rounded="50" elevation="5">
               <v-toolbar>
+                 <v-row>
                      <v-col cols="6" class="justify-start">
                             <v-toolbar-title>filtrer par date: </v-toolbar-title>
                      </v-col>
-                     <v-spacer></v-spacer>
-                     <v-col cols="7" class="justify-end">
+                            <v-spacer></v-spacer>
+                     <v-col cols="6" class="justify-end">
                             <v-btn @click="isDateFilterClose = !isDateFilterClose">
                                    <emojis :icon-path="iconPath" :icon-color="iconColor" width="40" height="40"></emojis>
                             </v-btn>
                      </v-col>
+                 </v-row>
               </v-toolbar>
-              <v-row>
-                     <v-col cols="8">
-                            <DateFilter v-if="isDateFilterClose" :date-picker-color="datePickerColor"
-                                   :is-date-filter-close="isDateFilterClose">
-                            </DateFilter>
-                     </v-col>
-                     <v-col cols="8">
-                            <DateFilter v-if="isDateFilterClose" :date-picker-color="datePickerColor"
-                                   :is-date-filter-close="isDateFilterClose">
-                            </DateFilter>
-                     </v-col>
-              </v-row>
+              <v-container>
+                     <v-row no-gutters class="justify-center">
+                            <v-col cols="6" v-for="picker in pickers" :key="picker.id">
+                                   <DateFilter :header="picker.titlePicker" :date-picker-color="datePickerColor" :date="date" :titlePicker="picker.titlePicker" :id="picker.id"
+                                   v-if="isDateFilterClose" :is-date-filter-close="isDateFilterClose" class="ma-2 pa-2">
+                                   </DateFilter>
+                            </v-col>
+                     </v-row>
+              </v-container>
        </v-card>
 </template>
 <script setup>
@@ -31,11 +30,23 @@ import DateFilter from '../components/DateFliter.vue'
 import { ref } from 'vue'
 
 //----------------DATA------------------
-// datePickerHandler
+//--------------------about pickers---------------------
+const pickers = [
+{idPicker: 0, titlePicker:'début'},
+{idPicker: 1, titlePicker:'fin'} 
+]
+console.log('aboutPickers', pickers)
+
+
+
 const date = ref("")
-console.log('[MENUHEADER], date', date.value)
+console.log('[component],[menuHeader], date', date.value)
 const isDateFilterClose = ref(false)
 const datePickerColor = ref('orange')
+
+// const dateUpdate= ref()
+// console.log('dateUpdate', dateUpdate.value)
+
 
 //-----------PROPS--------------
 const props = defineProps({
